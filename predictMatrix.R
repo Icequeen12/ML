de68316b8b8cbeb427feb08d0927a9378275a2bf
Pam <pamelapokladowska@gmail.com>
2020-05-29 23:38:00 +0200

    file change

---
+++ predictMatrix.R
@@ -0,0 +1,25 @@
+library(keras)
+library(tensorflow)
+
+model <- load_model_hdf5("C:/Users/Admin/Desktop/baza/fruits_checkpoints.h5")
+
+img_path <- "C:/Users/Admin/Desktop/testownik/"
+
+valid_data_gen <- image_data_generator(
+  rescale = 1/255)
+
+target_size <- c(20,20)
+
+valid_image_array_gen <- flow_images_from_directory(img_path, 
+                                                    valid_data_gen,
+                                                    target_size = target_size,
+                                                    class_mode = "categorical",
+                                                    shuffle = FALSE,
+                                                    classes = fruit_list,
+                                                    seed = 42)
+
+pred <- model %>% predict_generator(valid_image_array_gen,1)
+
+pred <- format(round(pred, 2), nsamll = 4)
+
+write.table(pred, file = "matrixFruits.txt", sep = "\t", row.names = FALSE)
